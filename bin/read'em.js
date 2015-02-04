a381551f578b246b7b2809fbedf96ee0aaf4fc6b
Joseph Orbegoso Pea <joe@trusktr.io>
2015-02-03 21:56:08 -0800

    Functionalized everything, added jsdocs. Started genDocs function.

--- bin/read'em.js
+++ bin/read'em.js
@@ -8,18 +8,47 @@ var atat       = require('at-at')
 var fs         = require('fs')
 var callAfter  = require('army-knife/callAfter').default
 var browserify = require('browserify')()
+var rndm       = require('rndm')
+var mkdirp     = require('mkdirp')
+var ejs        = require('ejs')
 
-app.version('0.0.1')
-    .option('-s, --source <source>', 'The source file or directory.')
-    .option('-o, --output <destination>', 'The output directory.')
-    .option('-p, --port <port>', 'The port to serve the docs on.')
-    .parse(process.argv)
+/**
+ * Get's the version of readem
+ *
+ * @param {Function} callback Gets called after the version is gotten, passed a
+ * signle argument: the version as a semver string.
+ */
+function getVersion(callback) {
+    var packageJson = [path.dirname(path.dirname(require.main.filename)),
+        'package.json'] .join('/')
 
-if (app.source) {
-    app.source = path.resolve(app.source)
+    fs.readFile(packageJson, function(err, data) {
+        if (err) throw new Error(err)
+        callback(JSON.parse(data).version)
+    });
 }
-else {
-    app.source = process.env.PWD
+
+/**
+ * Set's up the CLI, using commander.
+ *
+ * @param {string} version The version that gets shown at the command line by
+ * commander.
+ */
+function setUpCli(version) {
+    app.version(version)
+        .option('-s, --source <source>',
+                'The source file or directory to generate docs for.',
+                'src')
+        .option('-o, --output <destination>', 'The output directory.')
+        .option('-p, --port <port>', 'The port to serve the docs on.', 1134)
+        .parse(process.argv)
+
+    if (app.source) app.source = path.resolve(app.source)
+    else app.source = process.env.PWD
+
+    if (!app.output)
+        do app.output = ['/tmp/readem', rndm(24)].join('/')
+        while (fs.existsSync(path.resolve(app.output)))
 }
 
 /**
@@ -27,28 +56,35 @@ else {
  *
  * @param {string} dir A directory containing source files that we wish to
  * generate dox JSON for.
- * @param {Function} callback The callback function to call after docs are
- * parsed into JSON format.
+ * @param {Function} callback Gets called after dox are ready, receiving a
+ * single argument: the JSON dox, or null if no files were available to parse.
  */
-function genDocs(dir, callback) {
-    var docs = [] // contains the dox results for each file.
+function getDox(dir, callback) {
+    var docs = [] // contains the dox results of each file.
 
     // TODO: upgrade at-at so that it takes a filter, and read docs for all
     // files that match the filter, not just .js files.
     atat.walk(dir, function(files) {
+
+        // filter the files in the dir so we have only files ending with .js
         files = files.filter(function(file) {
             var extension = 'js'
-            return !!file.match(new RegExp('^.*\.'+extension+'$'))
+            return !!file.match(new RegExp('^.*\\.'+extension+'$'))
         })
 
-        callback = callAfter(files.length, callback).bind(this)
+        if (files.length)
+            callback = callAfter(files.length, callback).bind(this)
+        else console.log('No files found!'), callback(null)
 
         files.forEach(function(file) {
             fs.readFile(file, function(err, data) {
+                if (err) throw new Error(err)
+
                 docs.push({
                     file: file,
                     comments: dox.parseComments(data.toString())
                 })
+
                 callback(docs)
             })
         })
@@ -58,19 +94,19 @@ function genDocs(dir, callback) {
 /**
  * Serves the current directory on the given port.
  *
+ * @param {string} dir The directory to serve
  * @param {number} port The integer port to serve.
  */
-function serve(port) {
-
+function serve(dir, port) {
     var server = spawn(path.resolve(__dirname, '../node_modules/.bin/serve'),
-                       ['-p', (port ? port : '')])
+                       [dir, '-p', port])
 
     server.stdout.on('data', function(data) {
-        console.log('server stdout:', data.toString())
+        console.log(data.toString())
     })
 
     server.stderr.on('data', function(data) {
-        console.log('server sterr:', data.toString())
+        console.log('server err:', data.toString())
     })
 
     server.on('close', function(code) {
@@ -78,6 +114,36 @@ function serve(port) {
     })
 }
 
-genDocs(app.source, function(docs) {
-    serve(app.port)
+/**
+ * Generate documentation based on the dox of some source code.
+ *
+ * @param {Array} dox An array of dox results, each item the dox parsed from a
+ * file.
+ * @param {Object} options Options for generating the docs.
+ * @param {Object} options.dox An array of dox to generate dox from.
+ * @param {Object} options.template The name of a file that is an EJS template,
+ * used for the generated page of each source file.
+ * @param {string} dest The destination folder to place the generated docs
+ * files in.
+ * @param {Function} callback Called when docs are done being made.
+ */
+function genDocs(dox, options, callback) {
+    //mkdirp.sync(dest)
+    console.log(path.resolve(templateFile))
+
+    dox.forEach(function(dox) {
+        console.log('got a dox!')
+    })
+
+    callback()
+}
+
+getVersion(function(version) {
+    setUpCli(version)
+
+    getDox(app.source, function(dox) {
+        if (dox) genDocs(dox, '../src/js', app.output, function() {
+            serve(app.output, app.port)
+        })
+    })
 })
