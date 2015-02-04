005aa26d617209d3ea247f36c1b94dfb8dd230a4
Joseph Orbegoso Pea <joe@trusktr.io>
2015-02-04 00:56:30 -0800

    Initial genDocs function complete: a template is rendered to the output folder for each file of the source folder. Each template gets src/js/app.js embedded into it.

--- bin/read'em.js
+++ bin/read'em.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+console.log(' --- Generating docs...')
 
 var app        = require('commander')
 var dox        = require('dox')
@@ -7,10 +8,14 @@ var path       = require('path')
 var atat       = require('at-at')
 var fs         = require('fs')
 var callAfter  = require('army-knife/callAfter').default
-var browserify = require('browserify')()
 var rndm       = require('rndm')
 var mkdirp     = require('mkdirp')
 var ejs        = require('ejs')
+
+var browserify = require('browserify')
+var to5ify     = require('6to5ify')
+var famousify  = require('famousify')
+var cssify     = require('cssify')
 
 /**
  * Get's the version of readem
@@ -29,26 +34,32 @@ function getVersion(callback) {
 }
 
 /**
- * Set's up the CLI, using commander.
+ * Set's up the CLI, using commander to get specified command line arguments,
+ * and setting defaults for arguments that aren't supplied.
  *
  * @param {string} version The version that gets shown at the command line by
  * commander.
  */
 function setUpCli(version) {
     app.version(version)
-        .option('-s, --source <source>',
-                'The source file or directory to generate docs for.',
-                'src')
-        .option('-o, --output <destination>', 'The output directory.')
-        .option('-p, --port <port>', 'The port to serve the docs on.', 1134)
+        .option('-s, --source <source>',        'The directory containing source files to generate docs for. Defaults to ./src.')
+        .option('-o, --output <destination>',   'The output directory for the docs. Defaults to a random folder in /tmp/readem.')
+        .option('-t, --template <destination>', 'The template to use. Defaults to readem\'s own template.')
+        .option('-p, --port <port>',            'The port on which the output folder will be served. Defaults to 1134.', 1134)
         .parse(process.argv)
 
+    // If source isn't specified, use the current working directory.
     if (app.source) app.source = path.resolve(app.source)
     else app.source = process.env.PWD
 
-    if (!app.output)
-        do app.output = ['/tmp/readem', rndm(24)].join('/')
+    // If the output dir isn't specified, choose a random one that doesn't
+    // already exist in /tmp/readem.
+    if (app.output) app.output = path.resolve(app.output)
+    else do app.output = ['/tmp/readem', rndm(24)].join('/')
         while (fs.existsSync(path.resolve(app.output)))
+
+    if (!app.template)
+        app.template = path.resolve(__dirname, '../src/ejs/app.ejs')
 }
 
 /**
@@ -117,32 +128,66 @@ function serve(dir, port) {
 /**
  * Generate documentation based on the dox of some source code.
  *
- * @param {Array} dox An array of dox results, each item the dox parsed from a
- * file.
+ * @param {Array} dox An array of dox results, each item the dox parsed from a file.
  * @param {Object} options Options for generating the docs.
  * @param {Object} options.dox An array of dox to generate dox from.
- * @param {Object} options.template The name of a file that is an EJS template,
- * used for the generated page of each source file.
- * @param {string} dest The destination folder to place the generated docs
- * files in.
+ * @param {Object} options.template The name of a file that is an EJS template, used for the generated page of each source file.
+ * @param {Object} options.dest The directory where the docs are outputted to.
+ * @param {Object} options.source The source dir of the documented files.
  * @param {Function} callback Called when docs are done being made.
  */
 function genDocs(dox, options, callback) {
-    //mkdirp.sync(dest)
-    console.log(path.resolve(templateFile))
 
-    dox.forEach(function(dox) {
-        console.log('got a dox!')
+    // make the output directory.
+    mkdirp.sync(options.dest)
+
+    fs.readFile(options.template, function(err, data) {
+        if (err) throw new Error(err)
+
+        dox.forEach(function(dox) {
+            var appFile = path.resolve(__dirname, '../src/js/app.js')
+            var b = browserify()
+
+            // TODO: accept an app file via the command line. And browserify
+            // transforms too?
+            b.add(appFile)
+            .transform(to5ify)
+            .transform(famousify)
+            .transform(cssify)
+            .bundle(function(err, app) {
+                if (err) throw new Error('Error bundling app file '+appFile+'.')
+
+                var output = ejs.render(data.toString(), {
+                    dox: dox,
+                    app: app
+                })
+
+                // Mirror the structure of the source directory in the output directory.
+                var relativeFile = dox.file.replace(options.source+'/', '')
+                relativeFile = [relativeFile, 'html'].join('.')
+                mkdirp.sync([options.dest, path.dirname(relativeFile)].join('/'))
+
+                fs.writeFile([options.dest, relativeFile].join('/'), output, function(err) {
+                    if (err) throw new Error('Error writing doc file for '+dox.file+'.')
+                    callback()
+                })
+            })
+        })
     })
-
-    callback()
 }
 
 getVersion(function(version) {
     setUpCli(version)
 
     getDox(app.source, function(dox) {
-        if (dox) genDocs(dox, '../src/js', app.output, function() {
+        if (dox) genDocs(dox, {
+            dox: dox,
+            template: app.template,
+            dest: app.output,
+            // TODO store source and output in a class?
+            source: app.source
+        }, function() {
+            console.log(' --- Done.')
             serve(app.output, app.port)
         })
     })
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
 import 'famous/core/famous.css'
- 
+
 import Plane from 'infamous/Plane'
 import {contextWithPerspective} from 'infamous/utils'
- 
+
 var ctx = contextWithPerspective(1000)
 var square = new Plane({
     size: [200,200],
-    content: 'Hello.',
+    content: JSON.stringify(window.dox),
     properties: {
         backfaceVisibility: 'visible',
         background: 'pink',
         padding: '5px'
     }
 })
- 
+
 ctx.add(square)
 square.transform.setRotate([0,2*Math.PI,0], {duration: 5000, curve: 'easeInOut'})
