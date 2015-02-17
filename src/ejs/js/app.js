7c5fb3e276384736a8e3a2d3cc6c61a7e5e6e46c
Joseph Orbegoso Pea <joe@trusktr.io>
2015-02-16 18:32:36 -0800

    Temporary commit for thealphanerd.

--- src/ejs/js/app.js
+++ src/ejs/js/app.js
@@ -1,18 +1,21 @@
-import 'famous/core/famous.css'
+require('famous/core/famous.css')
+require('highlight.js/styles/solarized_light.css')
 
-import Plane from 'infamous/Plane'
-import PushMenuLayout from 'infamous/PushMenuLayout'
-import {contextWithPerspective} from 'infamous/utils'
+var Plane                  = require('infamous/Plane').default
+var PushMenuLayout         = require('infamous/PushMenuLayout').default
+var contextWithPerspective = require('infamous/utils').contextWithPerspective
+
+var hljs = require('highlight.js')
 
 // TODO, use jss.
 document.body.style.background = 'white'
 document.body.style.padding = '0'
 document.body.style.margin = '0'
+document.body.style.fontFamily = 'sans-serif'
 
-var ctx = contextWithPerspective(1000)
+var ctx = contextWithPerspective(1001)
 
 var content = new Plane({
-    content: JSON.stringify(window.dox),
     properties: {
         backfaceVisibility: 'visible',
         background: 'white',
@@ -38,11 +41,31 @@ var layout = new PushMenuLayout({
     fadeEndColor: 'rgba(255,255,255,0.8)'
 })
 
-window.layout = layout
-
 layout.setContent(content)
 layout.setMenu(menu)
 
-console.log(layout)
-
 ctx.add(layout)
+
+function escapeHTML(html) {
+    var el = document.createElement('textarea')
+    el.innerHTML = html
+    return el.innerHTML
+}
+
+function unescapeHTML(html) {
+    var el = document.createElement('textarea')
+    el.innerHTML = html
+    return el.value
+}
+
+content.setContent(document.getElementById('content').innerText)
+setTimeout(function() {
+
+    var pres = document.querySelectorAll('code')
+    console.log(pres)
+
+    Array.prototype.forEach.call(pres, function(el) {
+        console.log(el)
+        //hljs.highlightBlock(el)
+    })
+}, 2000)
